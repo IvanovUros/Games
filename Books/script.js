fa3ff173424801f3f57140fcb3b73a1ff3c096ee
IvanovUros <urivanov03@gmail.com>
2022-12-26 12:04:19 +0100

    added loadBooks functions

--- Books/script.js
+++ Books/script.js
@@ -1,5 +1,6 @@
 const newBooksList = document.querySelector('.new-books-wrapper');
 const bestSellingBooksList = document.querySelector('.best-selling-books-wrapper');
+const allBooksBtn = document.getElementById('allBooksBtn');
 
 let booksLoaded = [];
 let x = 0
@@ -21,6 +22,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         console.log(e);
     }
     let booksList = booksLoaded.record.results
+    localStorage.setItem('availableBooks', JSON.stringify(booksList))
     let highestRating = [];
     booksList.sort ((a,b) => b.rating - a.rating)
     console.log(booksList);
@@ -40,14 +42,57 @@ document.addEventListener("DOMContentLoaded", async () => {
                     <img src="./trolley.png" alt="" class="icon">
                 </div>
             </div>
-        </li>`;    
+        </li>`;
+        }
+        let bookWrapperList = Array.from(document.getElementsByClassName('book-wrapper'));
+        let imgWrapper = Array.from(document.getElementsByClassName('img-wrapper'));
+        let title = Array.from(document.getElementsByClassName('title'));
+        let priceWrapper = Array.from(document.getElementsByClassName('price-wrapper'));
+        let currentPrice;
+        for (let i = 0; i < 4; i++) {
+            let y = 0;
+            y = Math.floor(Math.random() * booksList.length);
+            imgWrapper[i].style.backgroundImage = `url(${booksList[y].img})`;
+            title[i].innerHTML += booksList[y].title;
+            currentPrice = Math.floor(Math.random() * (1000 - 100) + 100) / 100;
+            priceWrapper[i].innerHTML += currentPrice;
+            bookWrapperList[i].addEventListener('click',() => {
+                localStorage.setItem('currentObject', JSON.stringify( booksList[y]))
+            })
+        }
     }
-    let imgWrapper = Array.from(document.getElementsByClassName('.img-wrapper'));
-    let title = document.querySelector('.title');
-    let priceWrapper = document.querySelector('.price-wrapper');
+
+    function loadBestBooks () {
+        for (let i = 0; i < 4; i++) {
+            bestSellingBooksList.innerHTML += `<li class="book-wrapper--best-selling">
+            <div class="img-wrapper--best-selling"></div>
+            <div class="title--best-selling"></div>
+            <div class="bottom-wrapper">
+                <div class="price-wrapper--best-selling">$</div>
+                <div class="icon-wrapper">
+                    <img src="./trolley.png" alt="" class="icon">
+                </div>
+            </div>
+        </li>`;
+        }
+        let bookWrapperList = Array.from(document.getElementsByClassName('book-wrapper--best-selling'));
+        let imgWrapper = Array.from(document.getElementsByClassName('img-wrapper--best-selling'));
+        let title = Array.from(document.getElementsByClassName('title--best-selling'));
+        let priceWrapper = Array.from(document.getElementsByClassName('price-wrapper--best-selling'));
+        for (let i = 0; i < 4; i++) {
+            imgWrapper[i].style.backgroundImage = `url(${highestRating[i].img})`;
+            title[i].innerHTML += highestRating[i].title;
+            currentPrice = Math.floor(Math.random() * (1000 - 100) + 100) / 100;
+            priceWrapper[i].innerHTML += currentPrice;
+            bookWrapperList[i].addEventListener('click',() => {
+                console.log(highestRating[i]);
+                localStorage.setItem('currentObject', JSON.stringify( highestRating[i]))
+            })
+        }
     }
 
     loadNewBooks()
+    loadBestBooks()
 });
 
 
