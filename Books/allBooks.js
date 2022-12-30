d875d8d5c82856f30037652991687e355db3de91
IvanovUros <urivanov03@gmail.com>
2022-12-30 10:46:32 +0100

    added genre filter

--- Books/allBooks.js
+++ Books/allBooks.js
@@ -3,6 +3,7 @@ let booksAvailable = JSON.parse(localStorage.getItem('availableBooks')) || [];
 const availableBooksList = document.querySelector('.available-books-wrapper');
 const genreList = document.querySelector('.genre-list');
 let availableGenre = [];
+let genreWrapperList;
 
 fetchGenre = () => {
     let test = [];
@@ -17,15 +18,53 @@ fetchGenre = () => {
         <p class="genre-text">${availableGenre[i]}</p>
     </li>`;
     }
+    genreWrapperList = Array.from(document.getElementsByClassName('genre-wrapper'));
+    let genreArray;
+    for (let i = 0; i < availableGenre.length; i++) { 
+        genreWrapperList[i].addEventListener('click', () => {
+            genreArray = [];
+            booksAvailable.forEach(elem => {
+                availableBooksList.innerHTML = '';
+                if (elem.genre.includes(availableGenre[i])) {
+                    genreArray.push(elem);
+                }
+            });
+            genreArray.forEach(elem => {
+                availableBooksList.innerHTML += `<li class="book-wrapper">
+                <div class="img-wrapper"></div>
+                <div class="title"></div>
+                <div class="bottom-wrapper">
+                <div class="price-wrapper">$</div>
+                    <div class="icon-wrapper">
+                    <img src="./trolley.png" alt="" class="icon">
+                    </div>
+                    </div>
+                    </li>`;
+            })
 
-    let genreWrapperList = Array.from(document.getElementsByClassName('genre-wrapper'));
-    genreWrapperList.forEach(elem => {
-        elem.addEventListener('click', () => {
-            console.log(1)
-        })
-    })
+            let bookWrapperList = Array.from(document.getElementsByClassName('book-wrapper'));
+            let imgWrapper = Array.from(document.getElementsByClassName('img-wrapper'));
+            let title = Array.from(document.getElementsByClassName('title'));
+            let priceWrapper = Array.from(document.getElementsByClassName('price-wrapper'));
+            console.log(genreArray);
+
+            for (let y = 0; y < genreArray.length; y++) {
+                imgWrapper[y].style.backgroundImage = `url(${genreArray[y].img})`;
+                title[y].innerHTML += genreArray[y].title;
+                currentPrice = Math.floor(Math.random() * (1000 - 100) + 100) / 100;
+                priceWrapper[y].innerHTML += currentPrice;
+                bookWrapperList[y].addEventListener('click',() => {
+                    localStorage.setItem('currentBook',JSON.stringify(genreArray[y]))
+                })
+                bookWrapperList.forEach(elem => {
+                    elem.addEventListener('click',() => {
+                        window.location.assign('bookInfo.html')
+                    })
+                })
+            }
+        });
+    }
 }
-fetchGenre();
 
 homeBtn.addEventListener('click',() => {
     window.location.assign('index.html');
@@ -36,13 +75,15 @@ for (let i = 0; i < booksAvailable.length; i++) {
     <div class="img-wrapper"></div>
     <div class="title"></div>
     <div class="bottom-wrapper">
-        <div class="price-wrapper">$</div>
+    <div class="price-wrapper">$</div>
         <div class="icon-wrapper">
-            <img src="./trolley.png" alt="" class="icon">
+        <img src="./trolley.png" alt="" class="icon">
         </div>
-    </div>
-</li>`;
+        </div>
+        </li>`;
 }
+
+fetchGenre();
 
 let bookWrapperList = Array.from(document.getElementsByClassName('book-wrapper'));
 let imgWrapper = Array.from(document.getElementsByClassName('img-wrapper'));
@@ -58,6 +99,7 @@ for (let i = 0; i < bookWrapperList.length; i++) {
         localStorage.setItem('currentBook',JSON.stringify(booksAvailable[i]))
     })
 }
+
 
 bookWrapperList.forEach(elem => {
     elem.addEventListener('click',() => {
